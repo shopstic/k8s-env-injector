7231ef4c426ceeb1b76afadc3e2f24f6316d621d
Jacky Nguyen <nktpro@gmail.com>
2022-01-25 13:24:40 -0500

    Update lock.json

--- src/deps/fast-json-patch.ts
+++ src/deps/fast-json-patch.ts
@@ -1,1 +1,2 @@
-export * as jsonPatch from "https://cdn.skypack.dev/fast-json-patch@3.0.0-1?dts";
+// @deno-types="https://cdn.skypack.dev/pin/fast-json-patch@v3.1.0-ucpIg6BT1Zh8Gc3ZcsDC/dist=es2020,mode=types/index.d.ts"
+export * as jsonPatch from "https://cdn.skypack.dev/pin/fast-json-patch@v3.1.0-ucpIg6BT1Zh8Gc3ZcsDC/dist=es2020,mode=imports/optimized/fast-json-patch.js";
