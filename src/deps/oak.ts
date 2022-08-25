9f67f37050956b7b0ce7c42cc7c1fdb5e8acbbf6
Jacky Nguyen <nktpro@gmail.com>
2022-08-25 16:59:31 -0400

    Support nodeSelector in init-job

--- src/deps/oak.ts
+++ src/deps/oak.ts
@@ -2,6 +2,6 @@ export {
   Application,
   helpers as oakHelpers,
   Router,
-} from "https://deno.land/x/oak@v10.1.0/mod.ts";
+} from "https://deno.land/x/oak@v10.2.0/mod.ts";
 
-export type { ListenOptions } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+export type { ListenOptions } from "https://deno.land/x/oak@v10.2.0/mod.ts";
