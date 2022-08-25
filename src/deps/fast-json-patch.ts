bd094ff15fdefaebe3fb8119e1f14d461ad35c34
Jacky Nguyen <nktpro@gmail.com>
2022-08-25 17:26:38 -0400

    Update lock.json

--- src/deps/fast-json-patch.ts
+++ src/deps/fast-json-patch.ts
@@ -1,2 +1,2 @@
-// @deno-types="https://cdn.skypack.dev/pin/fast-json-patch@v3.1.0-ucpIg6BT1Zh8Gc3ZcsDC/dist=es2020,mode=types/index.d.ts"
-export * as jsonPatch from "https://cdn.skypack.dev/pin/fast-json-patch@v3.1.0-ucpIg6BT1Zh8Gc3ZcsDC/dist=es2020,mode=imports/optimized/fast-json-patch.js";
+// @deno-types="https://cdn.shopstic.com/pin/fast-json-patch@v3.1.0-ucpIg6BT1Zh8Gc3ZcsDC/dist=es2020,mode=types/index.d.ts"
+export * as jsonPatch from "https://cdn.shopstic.com/pin/fast-json-patch@v3.1.0-ucpIg6BT1Zh8Gc3ZcsDC/dist=es2020,mode=imports/optimized/fast-json-patch.js";
